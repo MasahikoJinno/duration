e064ee4a403d8c818fae7b86e198104127f0c0f8
Masahiko Jinno <mjinno@mbp14.lan>
2022-03-31 22:34:16 +0900

    feat: duration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use chrono::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let (b, a) = get_time_from_args(&args);
+    let duration = calc_duration_hour(b, a);
+    println!("duration: {}", duration)
+}
+
+fn convert_datetime_from_string(s: &str) -> DateTime<Local>{
+    let n: DateTime<Local> = Local::now();
+    let mut iter = s.splitn(2, ":");
+    let temp_hour: u32 = iter.next().unwrap().parse().unwrap();
+    let minutes: u32 = iter.next().unwrap().parse().unwrap();
+    let i = if temp_hour > 24 { 1 } else { 0 };
+    let hour: u32 = if temp_hour > 24 { temp_hour - 24 } else { temp_hour };
+    let dt = Local
+        .ymd(n.year(), n.month(), n.day() + i)
+        .and_hms(hour, minutes, 0);
+    dt
+}
+
+fn get_time_from_args(args: &Vec<String>) -> (DateTime<Local>, DateTime<Local>) {
+    (
+        convert_datetime_from_string(&args[1]),
+        convert_datetime_from_string(&args[2])
+    )
+}
+
+fn calc_duration_hour(before: DateTime<Local>, after: DateTime<Local>) -> f64 {
+    let duration = after - before;
+    duration.num_minutes() as f64 / 60.0
 }
